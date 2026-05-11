326b17f3292ff0770d2cb0e6838c10d3e5ea70a2
Rumeysa KAPLAN <rumeysaakaplan0@gmail.com>
2026-05-11 16:05:52 +0300

    Revert "Merge pull request #4 from rummeysaa/ui-desktop-ali"
    This reverts commit c6900d4fd0a82ee637e7805587a312e8a38a0c7f, reversing
    changes made to fb8d121765b95753b85428478ab333a3e0b56f70.

--- ui-desktop/src/main/java/com/odyometri/ui/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.odyometri.ui;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // JavaFX modül kontrolünü bypass edip uygulamayı başlatır
-        MainApp.main(args);
-    }
-}
